5f0839340ddc8989fe6cd68859a3f034d00f47ee
gary-269 <karanveersingh269@gmail.com>
2020-04-08 22:43:47 -0400

    Files Updated

--- client.c
+++ client.c
@@ -6,81 +6,95 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netdb.h>
+#include<signal.h>
+#include <time.h> 
+#include <sys/ipc.h>
+#include <arpa/inet.h>
 
-void player(char *s, int *fd1, int *fd2)
+void player(int server, int pid, char *playerName)
 {
-  int points=0;
-  int dice;
-  long int ss=0;
-  char turn;
-  while(1)
-  {
-    read(fd1[0], &turn, 1);
-    printf("%s: playing my dice\n", s);
-    dice =(int) time(&ss)%10 + 1;
-    printf("%s: got %d points\n", s, dice);
-    points+=dice;
-    printf("%s: Total so far %d\n\n", s, points);
-    if(points >= 100)
-    {
-      printf("%s: game over I won\n", s);
-      kill(0, SIGTERM);
+  int dice, n;
+  pid = fork();
+
+  if(pid)
+  { 
+    while(1)
+    { 
+      char message[255] = {0};
+      if((n=read(server, message, 255)))
+      {
+        fprintf(stderr,"%s\n", message);
+        if(!strcasecmp(message, "Game over: you won the game"))
+        {
+          fprintf(stderr,"I won the game\n");
+          kill(pid, SIGTERM);
+          exit(0);
+        }
+        else if(!strcasecmp(message, "Game over: you lost the game"))
+        {
+          fprintf(stderr,"I lost the game\n");
+          kill(pid, SIGTERM);
+          exit(0);
+        }  
+      }
+     }
+  }
+
+  else
+  {  
+    send(server , playerName , strlen(playerName), 0 ); 
+    while(1)
+    { 
+      sleep(2);
+      dice = (rand() % 6) + 1;
+      fprintf(stderr,"%s : got %d points\n\n", playerName, dice);
+      send(server, &dice, sizeof(dice), 0);
     }
-    sleep(5);    
-    write(fd2[1], &turn, 1);
   }
 }
-int main(int argc, char *argv[])
-{ 
-  char message[255] = {0};
-  int server, portNumber, pid, n;
-  socklen_t len;
+
+int socketConnector(char *ipAddress, char *port)
+{
+  int portNumber, server;
   struct sockaddr_in servAdd;
-  if(argc != 3)
-  {
-    printf("Call model: %s <IP> <Port #>\n", argv[0]);
-    exit(0);
-  }
   if((server=socket(AF_INET, SOCK_STREAM, 0))<0)
   {
     fprintf(stderr, "Cannot create socket\n");
-    exit(1); 
+    exit(1);
   }
   servAdd.sin_family = AF_INET;
-  sscanf(argv[2], "%d", &portNumber); 
-  servAdd.sin_port = htons((uint16_t)portNumber);
-  if(inet_pton(AF_INET, argv[1],&servAdd.sin_addr)<0)
+  sscanf(port, "%d", &portNumber);
+  servAdd.sin_port = htons((uint16_t)portNumber); 
+
+  if(inet_pton(AF_INET, ipAddress,&servAdd.sin_addr)<0)
   { 
-    fprintf(stderr, " inet_pton() has failed\n"); 
+    fprintf(stderr, " inet_pton() has failed\n");
     exit(2);
   }
-  if(connect(server, (struct sockaddr *) &servAdd,sizeof(servAdd))<0)
+  if(connect(server, (struct sockaddr *) &servAdd, sizeof(servAdd))<0)
   {
-    fprintf(stderr, "Unable to create connection, exiting\n");
+    fprintf(stderr, "connect() has failed, exiting\n");
     exit(3);
   }
-  read(server, message, 255);
-  fprintf(stderr, "message received: %s\n", message);
-  pid=fork();
-  if(pid)
-    while(1) /* reading server’s messages */
-      if(n=read(server, message, 255))
-      {
-        fprintf(stderr,"%s\n", message);
-        if(!strcasecmp(message, "Bye\n"))
-        {
-          kill(pid, SIGTERM);
-          exit(0); 
-        }
-      }
-  else while(1) // sending messages to server
-      if(n=read(0, message, 255))
-      {
-         write(server, message, strlen(message)+1);
-         if(!strcasecmp(message, "Bye\n"))
-        {
-          kill(getppid(), SIGTERM);
-          exit(0);
-        }
-       }
+
+  return server;
+}
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    int server, pid;
+    char welcomeMessage[255] = {0};
+    if(argc != 4)
+    {
+      printf("Call model: %s <Player Name> <IP> <Port #>\n", argv[0]);
+      exit(0);
+    }
+
+    server = socketConnector(argv[2], argv[3]);
+
+    read(server, welcomeMessage, 255);
+    fprintf(stderr, "%s\n\n", welcomeMessage);
+    sleep(2);
+    player(server, pid, argv[1]);
 }
--- server.c
+++ server.c
@@ -5,35 +5,82 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<signal.h>
 
-void servicePlayers(player1, player2)
+void servicePlayers(int player1, int player2)
 {
-  char message[255] = {0};
   int n, pid;
-  write(player1, "You can now play TOTO", 16);
-  if(pid=fork()) /* reading client messages */
-  while(1)
-    if(n=read(player1, message, 255))
+  char *gameOverMessage1, *gameOverMessage2;
+  char *welcomeMessage1, *welcomeMessage2;
+  char *turnMessage;
+  int dice;
+  char namePlayer1[255] = {0};
+  char namePlayer2[255] = {0};
+  int pointsPlayer1 = 0;
+  int pointsPlayer2 = 0;
+  int winningPoints = 100;
+
+  gameOverMessage1 = "Game over: You won the game";
+  gameOverMessage2 = "Game over: You lost the game";
+  welcomeMessage1 = "Welcome: Player1";
+  welcomeMessage2 = "Welcome: Player2";
+  turnMessage = "You can now play";
+
+  write(player1, welcomeMessage1, 16);
+  write(player2, welcomeMessage2, 16);
+
+  if((pid=fork()))
+  {
+    read(player1, namePlayer1, 255);
+    fprintf(stderr, "Player 1 name received: %s\n", namePlayer1);
+    read(player2, namePlayer2, 255);
+    fprintf(stderr, "Player 2 name received: %s\n", namePlayer2);
+    int roundNumber = 1;
+
+    while(1)
     {
-      fprintf(stderr,"%s\n", message);
-      if(!strcasecmp(message, "Bye\n"))
+      fprintf(stderr,"Round %d starts =>\n\n",roundNumber);
+      if(pointsPlayer1 <= winningPoints || pointsPlayer2 <= winningPoints)
       {
-      kill(pid, SIGTERM);
-      exit(0); 
+        send(player1, turnMessage, strlen(turnMessage), 0); 
+        send(player2, turnMessage, strlen(turnMessage), 0); 
       }
-    }
-  write(player2, "You can now play TITI", 16);
-  if(pid=fork()) /* reading client messages */
-  while(1)
-    if(n=read(player2, message, 255))
-    {
-      fprintf(stderr,"%s\n", message);
-      if(!strcasecmp(message, "Bye\n"))
+
+      if((n = read(player1, &dice, sizeof(int))))
+      { 
+        fprintf(stderr,"%s: playing dice\n",namePlayer1);
+        fprintf(stderr,"%s: got %d points\n",namePlayer1 ,dice);
+        pointsPlayer1+=dice;
+        fprintf(stderr,"%s: Total so far: %d\n\n",namePlayer1, pointsPlayer1);
+
+        if(pointsPlayer1 >= winningPoints)
+        {
+          write(player1, gameOverMessage1, strlen(gameOverMessage1));
+          write(player2, gameOverMessage2, strlen(gameOverMessage2));
+          kill(pid, SIGTERM);
+          kill(getppid(), SIGTERM);
+          exit(0); 
+        }
+      }
+  
+      if(n == read(player2, &dice, sizeof(int)))
       {
-      kill(pid, SIGTERM);
-      exit(0); 
-      }
+        fprintf(stderr,"%s: playing dice\n",namePlayer2);
+        fprintf(stderr,"%s: got %d points\n",namePlayer2 ,dice);
+        pointsPlayer2+=dice;
+        fprintf(stderr,"%s: Total so far: %d\n\n",namePlayer2, pointsPlayer2);
+        if(pointsPlayer2 >= winningPoints)
+        {
+          send(player2 , gameOverMessage1, strlen(gameOverMessage1), 0); 
+          send(player1 , gameOverMessage2, strlen(gameOverMessage2), 0); 
+          kill(pid, SIGTERM);
+          kill(getppid(), SIGTERM);
+          exit(0); 
+        }
+     }
+     roundNumber++;
     }
+  }
 }
 
 int socketcreator(char *port)
@@ -81,10 +128,11 @@ int main(int argc, char *argv[])
    printf("Got both players\n");
 
    if(!fork())
-    servicePlayers(player1, player2);
+      servicePlayers(player1, player2);
     close(player1);
     close(player2);
     waitpid(0, &status, WNOHANG);
   }
+  exit(0);
 }
     
